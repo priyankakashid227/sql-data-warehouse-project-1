05b62570d403512be4a490f853f683450200aa2b
Priyanka Kashid <priyankakashid227@gmail.com>
2026-04-11 14:04:02 +0530

    Create 06_ranking_analysis.sql

---
+++ scripts/analysis/06_ranking_analysis.sql
@@ -0,0 +1,29 @@
+/*====================================================
+ Script Name: 06_ranking_analysis.sql
+
+ Purpose:
+ - Identify top and bottom performers
+
+ Description:
+ - Rank products based on revenue
+====================================================*/
+
+-- Top 5 products
+SELECT TOP 5
+    p.product_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+-- Bottom 5 products
+SELECT TOP 5
+    p.product_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue ASC;
